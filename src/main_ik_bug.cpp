5689aabb90464ed1ccced9373eba89fde2cc9da6
onlyuser <onlyuser@gmail.com>
2017-08-16 01:20:49 +0800

    waypoint ckin

--- src/main_ik_bug.cpp
+++ src/main_ik_bug.cpp
@@ -219,13 +219,13 @@ int init_resources()
         (*p)->set_bump_texture_index((*p)->get_material()->get_texture_index_by_name("chesterfield_normal"));
         (*p)->set_ambient_color(glm::vec3(0));
         if(!leg_segment_index) {
-            (*p)->set_enable_joint_constraints(glm::ivec3(1, 1, 0));
-            (*p)->set_joint_constraints_center(glm::vec3(0, -45, 0));
+            (*p)->set_enable_joint_constraints(glm::ivec3(1, 1, 1));
+            (*p)->set_joint_constraints_center(glm::vec3(0, 0, 0));
             (*p)->set_joint_constraints_max_deviation(glm::vec3(0, 0, 0));
         } else {
             (*p)->set_enable_joint_constraints(glm::ivec3(1, 0, 1));
-            (*p)->set_joint_constraints_center(glm::vec3(0, 45, 0));
-            (*p)->set_joint_constraints_max_deviation(glm::vec3(0, 45, 0));
+            (*p)->set_joint_constraints_center(glm::vec3(0, 0, 0));
+            (*p)->set_joint_constraints_max_deviation(glm::vec3(0, 0, 0));
         }
         leg_segment_index++;
     }
